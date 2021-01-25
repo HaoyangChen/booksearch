8b1b058e1e0447eb4ac24e4b5b41fabc13ce648e
Haoyang Chen <haoyangchen0901@gmail.com>
2021-01-24 16:33:05 -0800

    Update Deployed URL

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # Info In, Book Out - Used Book Selling Web App
-## [Live Site](https://info340a-w20.github.io/project-iinbo/)
-You can access the live site of our application by visiting https://info340a-w20.github.io/project-iinbo/
+
+## [Live Site](https://haoyangchen.github.io/booksearch)
+
+You can access the live site of our application by visiting https://haoyangchen.github.io/booksearch
 
 ## Tools and Framework Used
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). We use Google's [Firebase](https://firebase.google.com/) to enable log in feature and store user information. We also utilized the [Material-UI](https://material-ui.com/) in our web application.
 
 ## Goal
+
 The overarching goal of our application is to help people to find the cheapest book they want to buy quickly. We hope that the sellers are able to quickly post a book that they want to sell through our customized search feature and buyers are able to quickly contact the sellers.
 
 ## API
+
 We used [Google Books API](https://developers.google.com/books/docs/v1/reference/bookshelves) to fetch book related information.
 
 ## Basic Usage
-You can click the category to view books with that specific category. You can search for any book in the search box at the search page. 
+
+You can click the category to view books with that specific category. You can search for any book in the search box at the search page.
 
 You can also make new post by first searching your book's name and author through the form. Then the search result will show up and you can select the "SELL ONE LIKE THIS" button to enter your customized description and address. Your post will then automatically be posted to the page. You can also delete a post you previously posted from that page.
 
@@ -31,7 +37,9 @@ The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
 ### `npm run deploy`
- Deploy the production version of the application to a gh-pages branch
 
- ## Copyright
- Copyright 2020 Haoyang (Eric) Chen, Yinan Guo
+Deploy the production version of the application to a gh-pages branch
+
+## Copyright
+
+Copyright 2020 Haoyang (Eric) Chen, Yinan Guo
--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,51 +1,61 @@
-import React from "react"
+import React from "react";
 // Firebase
-import 'firebase/auth';
-import 'firebase/database';
-import './AboutUs.css';
-import haoyangchenImg from './HaoyangChen.jpg';
-import yinanguoImg from './YinanGuo.jpg';
+import "firebase/auth";
+import "firebase/database";
+import "./AboutUs.css";
+import haoyangchenImg from "./HaoyangChen.jpg";
+import yinanguoImg from "./YinanGuo.jpg";
 
 const content = [
-    {
-      name: "Haoyang Chen",
-      title:
-        "Informatics Student at the University of Washington",
-      description: " I am a junior graduating in June, 2021 with a B.S in Informatics at the University of Washington. I am a passionate front-end web developer whose strength is in bringing technical skills and design ideas together to create user-friendly web applications. I found myself as a highly motivated, open-minded individual who can succeed in multi-cultural, challenging, and fast-paced environments. I have a strong passion for developing mobile-first, user-friendly websites. Through my previous internship experiences, I’ve developed my collaboration skills and learned how to enhance the user experience to satisfy consumer requirements in real-world business situations. My exposure to these experiences has shaped me to be a more astute thinker and motivated me to explore user experience and interaction design further.",
-      image: haoyangchenImg,
-      description2: "I hope to utilize my web development skills to develop well-organized web applications and to improve client relationships and business performance in various industries."
-    },
-    {
-      name: "Yinan Guo",
-      title:
-        "Informatics Student at the University of Washington",
-      description: " I am an undergraduate student pursuing a double degree in Mathematics and Informatics at the University of Washington. ",
-      description2: "My interest in science and technology rooted back to my high school study in New Hampshire. Since then, I have received one international award and three patents for my invention in technology and research in computer science. My current research interest lies in the interplay among information science, computer science and applied mathematics.",
-      image: yinanguoImg
-    }
-  ];
+  {
+    name: "Haoyang Chen",
+    title: "New Grad Software Engineer at University of Washington",
+    description:
+      "I am a new grad software engineer from the University of Washington. I specialize in full-stack software engineering (frontend/backend) and have built and deployed large-scale web and mobile applications for multiple enterprises, startups, and NGOs. I am currently looking for a full-time new grad Software Engineer position.",
+    image: haoyangchenImg,
+    description2:
+      "I am passionate about developing mobile-first, user-friendly web applications. Through my previous internship experiences, I've developed my collaboration skills and learned how to enhance user experience to satisfy consumer requirements in real-world business situations. My exposure to these experiences has shaped me to be a more astute thinker and motivated me to explore front end engineering further. I hope to utilize my skills to develop well-organized web apps and to improve client relationships and business performance in various industries.",
+  },
+  {
+    name: "Yinan Guo",
+    title: "Informatics Student at the University of Washington",
+    description:
+      " I am an undergraduate student pursuing a double degree in Mathematics and Informatics at the University of Washington. ",
+    description2:
+      "My interest in science and technology rooted back to my high school study in New Hampshire. Since then, I have received one international award and three patents for my invention in technology and research in computer science. My current research interest lies in the interplay among information science, computer science and applied mathematics.",
+    image: yinanguoImg,
+  },
+];
 
 export default class AboutUS extends React.Component {
-    render() {
-        return (
-            <div className="about-us">
-                <h1>About Us</h1>
-                {content.map((item, index) => (
-                    <div className="about-row" key={index}>
-                        <div className="col-sm-6">
-                            <div className="team-member">
-                                <img className="mx-auto rounded-circle" src={item.image} alt={item.name}></img>
-                                <p className="member-name">{item.name}</p>
-                                <p className="text-muted member-title">{item.title}</p>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 mx-auto justify-text">
-                            <p className="description-detail text-muted">{item.description}</p>
-                            <p className="description-detail text-muted">{item.description2}</p>
-                        </div>
-                    </div>
-                ))}
+  render() {
+    return (
+      <div className="about-us">
+        <h1>About Us</h1>
+        {content.map((item, index) => (
+          <div className="about-row" key={index}>
+            <div className="col-sm-6">
+              <div className="team-member">
+                <img
+                  className="mx-auto rounded-circle"
+                  src={item.image}
+                  alt={item.name}
+                ></img>
+                <p className="member-name">{item.name}</p>
+                <p className="text-muted member-title">{item.title}</p>
+              </div>
             </div>
-        )
-    }
+            <div className="col-sm-6 mx-auto justify-text">
+              <p className="description-detail text-muted">
+                {item.description}
+              </p>
+              <p className="description-detail text-muted">
+                {item.description2}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
